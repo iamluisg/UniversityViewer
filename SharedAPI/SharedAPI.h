c9754c8451249a43440f9ff3fed1d9782d2d685a
Luis Garcia <iam.luisg@gmail.com>
2021-11-18 17:35:48 -0600

    Add SharedAPI target

---
+++ SharedAPI/SharedAPI.h
@@ -0,0 +1,18 @@
+//
+//  SharedAPI.h
+//  SharedAPI
+//
+//  Created by Luis Garcia on 11/18/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SharedAPI.
+FOUNDATION_EXPORT double SharedAPIVersionNumber;
+
+//! Project version string for SharedAPI.
+FOUNDATION_EXPORT const unsigned char SharedAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SharedAPI/PublicHeader.h>
+
+
